2c698484365ecba214733440915525a58deceb2f
samer-bakeer <lord57881@gmail>
2024-11-29 19:01:04 +0300

    debugShowCheckedModeBanner: false

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       locale: WidgetsBinding.instance.window.locale,
       supportedLocales: [
         Locale('en', 'US'),
