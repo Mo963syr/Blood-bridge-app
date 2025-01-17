52af492c715130e0fb3112a1f210e9e3e2468b91
samer-bakeer <lord57881@gmail>
2025-01-17 11:47:07 +0300

    api createpost

--- frontend/lib/Awareness Coordinato/mainCoordinator.dart
+++ frontend/lib/Awareness Coordinato/mainCoordinator.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../home_page.dart';
 import 'package:intl/intl.dart';
+import '../services/user_preferences.dart';
+import 'dart:io';
+import 'package:dio/dio.dart';
 
 class AwarenessCoordinatorPage extends StatefulWidget {
   @override
@@ -10,25 +13,54 @@ class AwarenessCoordinatorPage extends StatefulWidget {
 }
 
 class _AwarenessCoordinatorPageState extends State<AwarenessCoordinatorPage> {
-  TextEditingController _titleController =
-      TextEditingController(); // للتحكم في العنوان
-  TextEditingController _controller = TextEditingController(); // للتحكم في النص
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _contectController = TextEditingController();
 
-  void _addPost() {
-    if (_controller.text.isNotEmpty && _titleController.text.isNotEmpty) {
-      DateTime now = DateTime.now();
-      String formattedDate = DateFormat('yyyy-MM-dd HH:mm').format(now);
-
-      // إضافة المنشور مع العنوان
-      Provider.of<ThemeProvider>(context, listen: false).addPost({
-        'title': _titleController.text, // أخذ العنوان من TextField
-        'text': _controller.text,
-        'timestamp': now, // حفظ الوقت عند النشر
+  TextEditingController _controller = TextEditingController();
+  Future<void> createpost(BuildContext context) async {
+    String? userId = await UserPreferences.getUserId();
+    if (userId == null) {
+      print('User ID not found');
+      return;
+    }
+    if (_titleController == null || _contectController == null) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("يرجى ملئ جميع الحقول")));
+      return;
+    }
+    try {
+      final dio = Dio();
+      final formData = FormData.fromMap({
+        'title': _titleController,
+        'content': _contectController,
+        'userId': userId,
       });
 
-      _titleController.clear(); // مسح حقل العنوان بعد الإضافة
-      _controller.clear(); // مسح حقل النص بعد الإضافة
-      Navigator.pop(context); // العودة إلى الصفحة السابقة بعد الإضافة
+      final response = await dio.post(
+        'http://10.0.2.2:8080/api/create-post',
+        data: formData,
+      );
+      print(response.statusCode);
+      print(response.data);
+      if (response.statusCode == 201) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(' successfully')),
+        );
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => AwarenessCoordinatorPage()),
+        );
+      } else {
+        print('Error: ${response.statusCode}');
+        print('Response: ${response.data}');
+      }
+    } catch (e) {
+      if (e is DioException) {
+        print('DioError: ${e.response?.statusCode}');
+        print('Error data: ${e.response?.data}');
+      } else {
+        print('Unexpected error: $e');
+      }
     }
   }
 
@@ -56,7 +88,7 @@ class _AwarenessCoordinatorPageState extends State<AwarenessCoordinatorPage> {
             SizedBox(height: 16),
             // حقل النص
             TextField(
-              controller: _controller,
+              controller: _contectController,
               maxLines: 5,
               decoration: InputDecoration(
                 hintText: 'اكتب منشور توعوي هنا...',
@@ -65,9 +97,11 @@ class _AwarenessCoordinatorPageState extends State<AwarenessCoordinatorPage> {
               ),
             ),
             SizedBox(height: 16),
-            // زر لنشر المنشور
+
             ElevatedButton(
-              onPressed: _addPost,
+              onPressed: () {
+                createpost(context);
+              },
               child: Text('إضافة منشور'),
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.red[400],
