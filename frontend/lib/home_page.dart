6344e736af1f14b9fdc7380a723e0e72befa5ed0
samer-bakeer <lord57881@gmail>
2025-02-06 04:18:59 +0300

    add setting

--- frontend/lib/home_page.dart
+++ frontend/lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:frontend/signup_page.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 import 'signin_page.dart';
@@ -7,7 +8,6 @@ import 'createrequest.dart';
 import 'profilepage.dart';
 import 'donationrequestpage.dart';
 import 'appointmentsUser.dart';
-import 'setting_page.dart';
 import 'services/user_preferences.dart';
 import 'package:frontend/requestForOther.dart';
 
@@ -44,6 +44,7 @@ class MyApp extends StatelessWidget {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           theme: themeProvider.themeData,
+          // يمكنك تعديل الصفحة الرئيسية إذا لزم الأمر (مثلاً صفحة تسجيل الدخول)
           home: HomePage(),
         );
       },
@@ -60,22 +61,16 @@ class _HomePageState extends State<HomePage> {
   int _selectedIndex = 2;
   bool isLoading = true;
   List<Map<String, dynamic>> post = [];
-  List<Map<String, dynamic>> count = [];
   int req = 0;
   int donrequestcount = 0;
   int requestForOtercount = 0;
+
   @override
   void initState() {
     super.initState();
     fetchPosts();
     reqCount();
   }
-
-  // @override
-  // void initStatem() {
-  //   super.initState();
-  //   reqCount();
-  // }
 
   Future<void> fetchPosts() async {
     try {
@@ -112,11 +107,11 @@ class _HomePageState extends State<HomePage> {
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
         setState(() {
-          req = data['requestcount'] ?? 'لا توجد بيانات';
-          donrequestcount = data['donrequestcount'] ?? 'لا توجد بيانات';
-          requestForOtercount = data['requestForOtercount'] ?? 'لا توجد بيانات';
-          print('reqcount :${req}');
-          print('userid :${userId}');
+          req = data['requestcount'] ?? 0;
+          donrequestcount = data['donrequestcount'] ?? 0;
+          requestForOtercount = data['requestForOtercount'] ?? 0;
+          print('reqcount :$req');
+          print('userid :$userId');
           print(response.statusCode);
         });
       } else {
@@ -138,12 +133,7 @@ class _HomePageState extends State<HomePage> {
       );
     } else if (index == 1) {
       reqCount();
-      // print(req);
       if (req >= 1) {
-        // Navigator.push(
-        //   context,
-        //   MaterialPageRoute(builder: (context) => HomePage()),
-        // );
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('لا يمكنك انشاء طلب جديد لديك طلب سابق')),
         );
@@ -155,11 +145,7 @@ class _HomePageState extends State<HomePage> {
       }
     } else if (index == 3) {
       reqCount();
-      if (donrequestcount.toInt() >= 1) {
-        // Navigator.push(
-        //   context,
-        //   MaterialPageRoute(builder: (context) => HomePage()),
-        // );
+      if (donrequestcount >= 1) {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('لا يمكنك انشاء طلب جديد لديك طلب سابق')),
         );
@@ -167,9 +153,6 @@ class _HomePageState extends State<HomePage> {
         Navigator.push(
           context,
           MaterialPageRoute(builder: (context) => DonationRequestPage()),
-        );
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('لا ')),
         );
       }
       if (req >= 1) {
@@ -184,16 +167,12 @@ class _HomePageState extends State<HomePage> {
       );
     } else if (index == 5) {
       reqCount();
-      if (requestForOtercount.toInt() >= 3) {
+      if (requestForOtercount >= 3) {
         print(requestForOtercount);
-        // Navigator.push(
-        //   context,
-        //   MaterialPageRoute(builder: (context) => HomePage()),
-        // );
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
               content: Text(
-                  'لا يمكنك انشاء طلب للغير جديد لديك ${requestForOtercount} طلبات سابقة')),
+                  'لا يمكنك انشاء طلب للغير جديد لديك $requestForOtercount طلبات سابقة')),
         );
       } else if (requestForOtercount < 3) {
         Navigator.push(
@@ -210,119 +189,134 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('الصفحة الرئيسية'),
-      ),
-      body: isLoading
-          ? Center(child: CircularProgressIndicator())
-          : post.isEmpty
-              ? Center(
-                  child: Text(
-                    'لاتوجد منشورات حالياً',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                )
-              : ListView.builder(
-                  padding: const EdgeInsets.all(8.0),
-                  itemCount: post.length,
-                  itemBuilder: (context, index) {
-                    final posts = post[index];
-                    return Directionality(
-                      textDirection: TextDirection.rtl,
-                      child: Card(
-                        margin: const EdgeInsets.symmetric(
-                            vertical: 10.0, horizontal: 15.0),
-                        elevation: 6,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            ListTile(
-                              leading: CircleAvatar(
-                                backgroundColor: Colors.grey[300],
-                                backgroundImage:
-                                    NetworkImage(posts['userImage'] ?? ''),
-                              ),
-                              title: Text(
-                                posts['username'] ?? 'مستخدم مجهول',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        appBar: AppBar(
+          // وضع أيقونة الإعدادات في أعلى الصفحة من اليسار
+          leading: IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SettingsPage()),
+              );
+            },
+          ),
+          title: Text('الصفحة الرئيسية'),
+          backgroundColor: Colors.red,
+        ),
+        body: isLoading
+            ? Center(child: CircularProgressIndicator())
+            : post.isEmpty
+                ? Center(
+                    child: Text(
+                      'لاتوجد منشورات حالياً',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  )
+                : ListView.builder(
+                    padding: const EdgeInsets.all(8.0),
+                    itemCount: post.length,
+                    itemBuilder: (context, index) {
+                      final posts = post[index];
+                      return Directionality(
+                        textDirection: TextDirection.rtl,
+                        child: Card(
+                          margin: const EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 15.0),
+                          elevation: 6,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              ListTile(
+                                leading: CircleAvatar(
+                                  backgroundColor: Colors.grey[300],
+                                  backgroundImage:
+                                      NetworkImage(posts['userImage'] ?? ''),
                                 ),
-                              ),
-                              subtitle:
-                                  Text('تم النشر منذ ${posts['time']} ساعة'),
-                              trailing: Icon(Icons.more_vert),
-                            ),
-                            if (posts['postImage'] != null)
-                              ClipRRect(
-                                borderRadius: BorderRadius.circular(12),
-                                child: Image.network(
-                                  posts['postImage']!,
-                                  height: 300,
-                                  width: double.infinity,
-                                  fit: BoxFit.cover,
+                                title: Text(
+                                  posts['username'] ?? 'مستخدم مجهول',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
+                                subtitle:
+                                    Text('تم النشر منذ ${posts['time']} ساعة'),
+                                trailing: Icon(Icons.more_vert),
                               ),
-                            Padding(
-                              padding: const EdgeInsets.all(12.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  SizedBox(height: 8),
-                                  Center(
-                                    child: Text(
-                                      posts['title'] ?? 'عنوان غير متوفر',
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
+                              if (posts['postImage'] != null)
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(12),
+                                  child: Image.network(
+                                    posts['postImage']!,
+                                    height: 300,
+                                    width: double.infinity,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              Padding(
+                                padding: const EdgeInsets.all(12.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(height: 8),
+                                    Center(
+                                      child: Text(
+                                        posts['title'] ?? 'عنوان غير متوفر',
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                        ),
                                       ),
                                     ),
-                                  ),
-                                  SizedBox(height: 5),
-                                  Text(
-                                    posts['content'] ?? 'محتوى غير متوفر',
-                                    style: TextStyle(
-                                      fontSize: 14,
-                                      color: Colors.black87,
+                                    SizedBox(height: 5),
+                                    Text(
+                                      posts['content'] ?? 'محتوى غير متوفر',
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        color: Colors.black87,
+                                      ),
+                                      textAlign: TextAlign.right,
                                     ),
-                                    textAlign: TextAlign.right,
-                                  ),
-                                  SizedBox(height: 8),
-                                  Row(
-                                    children: [
-                                      Icon(Icons.favorite_border),
-                                      SizedBox(width: 120),
-                                      Icon(Icons.comment_outlined),
-                                      SizedBox(width: 120),
-                                      Icon(Icons.send),
-                                    ],
-                                  ),
-                                ],
+                                    SizedBox(height: 8),
+                                    Row(
+                                      children: [
+                                        Icon(Icons.favorite_border),
+                                        SizedBox(width: 120),
+                                        Icon(Icons.comment_outlined),
+                                        SizedBox(width: 120),
+                                        Icon(Icons.send),
+                                      ],
+                                    ),
+                                  ],
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  },
-                ),
-      bottomNavigationBar: BottomAppBar(
-        color: Color(0xFFC62828),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 5.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              _buildNavItem(Icons.person, 'الملف الشخصي', 0),
-              _buildNavItem(Icons.add_circle, "طلب حاجة", 1),
-              _buildNavItem(Icons.home, 'الرئيسية', 2),
-              _buildNavItem(Icons.search, "طلب تبرع", 3),
-              _buildNavItem(Icons.history, 'مواعيد', 4),
-              _buildNavItem(Icons.favorite, "طلب لغيري", 5),
-            ],
+                      );
+                    },
+                  ),
+        bottomNavigationBar: BottomAppBar(
+          color: Color(0xFFC62828),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                _buildNavItem(Icons.person, 'الملف الشخصي', 0),
+                _buildNavItem(Icons.add_circle, "طلب حاجة", 1),
+                _buildNavItem(Icons.home, 'الرئيسية', 2),
+                _buildNavItem(Icons.search, "طلب تبرع", 3),
+                _buildNavItem(Icons.history, 'مواعيد', 4),
+                _buildNavItem(Icons.favorite, "طلب لغيري", 5),
+              ],
+            ),
           ),
         ),
       ),
@@ -350,6 +344,74 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  bool notificationsEnabled = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('الإعدادات'),
+          backgroundColor: Colors.red,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              SwitchListTile(
+                title: Text('تفعيل الإشعارات'),
+                value: notificationsEnabled,
+                onChanged: (value) {
+                  setState(() {
+                    notificationsEnabled = value;
+                  });
+                },
+              ),
+              Divider(),
+              ListTile(
+                title: Text(
+                  'المطورين:',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(
+                  'سامر باكير\nمحمد الحسين\nموفّق عقيد\n\nبإشراف مركز التبرع بالدم',
+                  style: TextStyle(height: 1.5),
+                ),
+              ),
+              Spacer(),
+              ElevatedButton.icon(
+                onPressed: () {
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => SigninPage()),
+                    (route) => false,
+                  );
+                },
+                icon: Icon(Icons.logout),
+                label: Text(
+                  'تسجيل الخروج',
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  minimumSize: Size(double.infinity, 50),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
